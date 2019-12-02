5704446d79c4f33d3e2dfd9bb235e894d5f0be25
nastya9998 <55386717+nastya9998@users.noreply.github.com>
2019-12-02 22:50:40 +0300

    Laboratory work, Kudryavtseva Anastasya 18fpl-1

--- lab_3/main.py
+++ lab_3/main.py
@@ -48,14 +48,61 @@ class WordStorage:
         #вызывает путAnn
 
 class NGramTrie:
+        def __init__(self, n):
+        self.gram_frequencies = {}
+        self.gram_log_probabilities = {}
+        self.size = n
+
     def fill_from_sentence(self, sentence: tuple) -> str:
-        pass
+        if not isinstance(sentence, tuple):
+            return 'ERROR'
+        for i in range(len(sentence)):
+            if i < len(sentence) - self.size:
+                words_gram = sentence[i: i + self.size]
+                if words_gram in self.gram_frequencies:
+                    self.gram_frequencies[words_gram] += 1
+                else:
+                    self.gram_frequencies[words_gram] = 1
+            elif i == len(sentence) - self.size:
+                words_gram = sentence[i:]
+                if words_gram in self.gram_frequencies:
+                    self.gram_frequencies[words_gram] += 1
+                else:
+                    self.gram_frequencies[words_gram] = 1
+        return 'OK'
 
     def calculate_log_probabilities(self):
-        pass
+        for pair in self.gram_frequencies:
+            w_n_1 = pair[0:self.size - 1]
+            count = 0
+            for key in self.gram_frequencies:
+                if w_n_1 == key[0:self.size - 1]:
+                    count += self.gram_frequencies[key]
+            prob = math.log(self.gram_frequencies[pair]/count)
+            self.gram_log_probabilities[pair] = prob
 
     def predict_next_sentence(self, prefix: tuple) -> list:
-        pass
+        if not isinstance(prefix, tuple) or not prefix:
+            return []
+        if len(prefix) != self.size - 1:
+            return []
+        sentence = []
+        prefix_list = list(prefix)
+        sentence.extend(prefix_list)
+        beginnings = []
+        for gram in self.gram_log_probabilities:
+            beginnings.append(gram[0: self.size - 1])
+        while prefix in beginnings:
+            pro = []
+            for k, v in self.gram_log_probabilities.items():
+                if prefix == k[0: self.size - 1]:
+                    pro.append((v, k))
+            pro.sort(reverse=True)
+            affix = pro[0][1][-1]
+            sentence.append(affix)
+            prefix = pro[0][1][1:]
+        return sentence
+
 
 
 def encode(storage_instance, corpus) -> list:
